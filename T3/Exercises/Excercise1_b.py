cf691769862183c76da2366dc21aeb812a2ac962
Daniel Muñoz <dmunoz@uloyola.es>
2024-10-23 14:56:46 +0200

    Adding T3 exercides

---
+++ T3/Exercises/Excercise1_b.py
@@ -0,0 +1,22 @@
+import random
+
+class Dice:
+    #To do
+        self.sides = sides
+
+    def roll(self):
+        #To do
+
+# Testing program
+#To do
+    dice1 = Dice()
+    dice2 = Dice()
+
+    for _ in range(num_rolls):
+        roll1 = dice1.roll()
+        #To do
+
+        #To do
+
+# Simulate rolling two dice five times each
+simulate_rolling_two_dice(5) 
